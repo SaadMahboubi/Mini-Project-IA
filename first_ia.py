31f632e84b1f26272dd35560faa17be749f8f15d
Saad Mahboubi <saad.mahboubi@etu.univ-nantes.fr>
2020-10-03 00:06:59 +0200

    fin mini projet a relire pour comprendre

--- first_ia.py
+++ first_ia.py
@@ -1,8 +1,9 @@
-import numpy as np
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#! usr/bin/python
+# -*- coding: ISO-8859-1 -*-
 
-x_entrer = np.array(([3,1.5],[2,1],[4,1.5],[3,1],[3.5,0.5],[2,0.5],[5.5,1],[1,1],[4.5,1]),dtype=float)
+import numpy as np
+
+x_entrer = np.array(([3,1.5],[2,1],[4,1.5],[3,1],[3.5,0.5],[2,0.5],[5.5,1],[1,1],[3,0]),dtype=float)
 y = np.array(([1],[0],[1],[0],[1],[0],[1],[0]),dtype=float) 
 
 x_entrer = x_entrer/np.amax(x_entrer, axis=0)
